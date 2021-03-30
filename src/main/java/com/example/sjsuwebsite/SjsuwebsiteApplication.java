288f8d9cb9b39950d82127938f00517dc356fdaa
Allen Baek <baekallen@gmail.com>
2021-03-29 21:11:17 -0700

    Initial commit

---
+++ src/main/java/com/example/sjsuwebsite/SjsuwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjsuwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjsuwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjsuwebsiteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sjsuwebsite/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.sjsuwebsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String MainPage(){
+        return "file";
+    }
+}
