f9c422f1eccd16a912d2fa7ad8f967a3ca26c1ff
Allen Baek <baekallen@gmail.com>
2021-05-09 18:39:41 -0700

    fix git not getting the updated ConfirmCheckout Page

--- src/main/java/com/example/sjsuwebsite/repository/HistoryRepository.java
+++ src/main/java/com/example/sjsuwebsite/repository/HistoryRepository.java
@@ -3,5 +3,8 @@ package com.example.sjsuwebsite.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.sjsuwebsite.model.History;
 
+import java.util.ArrayList;
+
 public interface HistoryRepository extends MongoRepository<History, String> {
+    public ArrayList<History> findAllByUsername(String name);
 }
