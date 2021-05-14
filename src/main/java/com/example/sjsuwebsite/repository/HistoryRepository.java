66b06e54109e03921f945115c90e1f069172b0c7
Allen Baek <baekallen@gmail.com>
2021-05-14 00:34:22 -0700

    Javadoc for HistoryRepository and UserRepository

--- src/main/java/com/example/sjsuwebsite/repository/HistoryRepository.java
+++ src/main/java/com/example/sjsuwebsite/repository/HistoryRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.sjsuwebsite.model.History;
 import java.util.ArrayList;
 
+/**
+ * Repository for interacting with Mongodb for History class
+ */
 public interface HistoryRepository extends MongoRepository<History, String> {
+    /**
+     *
+     * @param name
+     * takes in String name
+     *
+     * @return
+     * returns and ArrayList based on the name
+     */
     public ArrayList<History> findAllByUsername(String name);
 }
--- src/main/java/com/example/sjsuwebsite/repository/UserRepository.java
+++ src/main/java/com/example/sjsuwebsite/repository/UserRepository.java
@@ -3,8 +3,25 @@ package com.example.sjsuwebsite.repository;
 import com.example.sjsuwebsite.model.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * Repository with the Mongodb for User Class
+ */
 public interface UserRepository extends MongoRepository<Users, String> {
+    /**
+     * @param username
+     * gets username
+     * @return
+     * pushes a boolean variable depending if the username exists in the database
+     */
     public Boolean existsUsersByUsername(String username);
+
+    /**
+     * @param username
+     * gets username
+     * @param password
+     * gets password
+     * @return
+     * pushes a boolean variable depending if the username and the password exists in the database
+     */
     public Boolean existsUsersByUsernameAndPassword(String username,String password);
-//    public void changePassword(String username, String password);
 }
